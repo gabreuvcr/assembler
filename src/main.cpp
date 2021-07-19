0c37f3c2005393418e8429051c4f53532455386b
Caio Guedes <caioguedesam@gmail.com>
2021-07-19 15:49:37 -0300

    Lendo arquivo de teste

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include <map>
+#include <vector>
 #include <string>
 
 #include "montador.h"
@@ -25,6 +29,23 @@ bool is_op(std::string command) {
     return is_zero_op(command) || is_one_op(command) || is_two_op(command);
 }
 
+std::vector<std::string> get_input(const std::string& file_name) {
+	std::ifstream f;
+    f.open(file_name);
+
+    std::vector<std::string> inputs;
+	std::string line;
+	while (std::getline(f, line)) {
+        std::stringstream ss(line);
+        std::string command;
+        while(getline(ss, command, ' ')) {
+            inputs.push_back(command);
+        }
+	}
+    f.close();
+    return inputs;
+}
+
 int main(int argc, char *argv[]) {
     std::map<std::string, int> table = {
         {"HALT", 0}, {"LOAD", 1}, {"STORE", 2},
@@ -37,34 +58,50 @@ int main(int argc, char *argv[]) {
         {"R0", 0}, {"R1", 1}, {"R2", 2}, {"R3", 3}
     };
 
-    std::string commands[15] = { "READ", "R0", "LOAD", "R1", "const100", "ADD", "R0", "R1", "WRITE", "R0", "HALT", "const100:", "WORD", "100", "END"};
+    std::vector<std::string> commands = get_input(argv[1]);
 
-    int size = sizeof(commands) / sizeof(std::string);
+    int size = commands.size();
 
     int num_lines = 0;
+
     for (int i = 0; i < size; i++) {
-        if (is_op(commands[i])) {
+		int end_string = commands[i].size() - 1;
+
+		// Lendo label
+		if (commands[i][end_string] == ':') {
+			table[commands[i].substr(0, end_string)] = ++num_lines;
+			// Caso ap�s a label seja opera��o, conta a linha na pr�xima leitura
+			if (is_op(commands[i + 1])) num_lines--;
+
+			continue;
+		}
+
+		// Lendo comando
+		if (is_op(commands[i])) {
             num_lines++;
-            continue;
-        }
-
-        int end_string = commands[i].size() - 1;
-
-        if (commands[i][end_string] == ':') {
-            table[commands[i].substr(0, end_string)] = ++num_lines;
         }
     }
 
+	std::vector<std::string> output;
+
     for (int i = 0; i < size; i++) {
         if (commands[i] == "WORD") {
-            std::cout << commands[i + 1] << " ";
+			output.push_back(commands[i + 1]);
         }
         if (table.find(commands[i]) == table.end()) {
             continue;
         }
-        std::cout << table[commands[i]] << " ";
+		output.push_back(std::to_string(table[commands[i]]));
     }
 
+	int memory = output.size();
+	std::cout << memory << std::endl;
+	for (int i = 0; i < memory; i++) {
+		std::cout << output[i];
+		if (i != memory - 1) {
+			std::cout << " ";
+		}
+	}
     std::cout << std::endl;
     return 0;
 }
