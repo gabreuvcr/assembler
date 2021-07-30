5df481d07be09e464d1a25e8802587dbb0f82f5e
gabreuvcr <gabreuvcr@gmail.com>
2021-07-30 14:46:05 -0300

    Removendo função não utilizada do .h

--- include/montador.h
+++ include/montador.h
@@ -9,7 +9,6 @@
 #include <vector>
 #include <algorithm>
 
-void remove_tabs(std::string &in);
 std::vector<std::string> get_input(const std::string &file_name);
 void first_pass(std::map<std::string, int> &table, std::map<std::string, int> &labels, std::vector<std::string> &commands);
 std::vector<std::string> second_pass(std::map<std::string, int> table, std::map<std::string, int> labels, std::vector<std::string> commands);
